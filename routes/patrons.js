39a8e4f6bcb0f55b1544ba72efb1879ea6e293cb
Nick Elliott <nickmartinelliott@gmail.com>
2020-09-09 23:34:33 -0400

    Updated Patron Detail Page: submitted data will now update patron in database, submitting invalid data will rerender Patron Detail page with errors next to field of invalid data,user entered data will remain populated if form is submitted with invalid data and page rerenders

--- routes/patrons.js
+++ routes/patrons.js
@@ -3,20 +3,20 @@ const router = express.Router();
 const Patrons = require("../models").Patrons;
 const Loans = require("../models").Loans;
 
-// router.get('/', function(req, res, next) {
-//   res.render('all_patrons', {title: "Patrons"});
-// });
 
+// All Patrons page
 router.get('/', function(req, res, next) {
   Patrons.findAll({order: [["last_name", "DESC"]]}).then(function(patrons){
     res.render('all_patrons', {patrons: patrons, title: 'Patrons' });
   });
 });
 
+// New Patron page
 router.get('/new', function(req, res, next) {
   res.render('new_patron', {patrons: Patrons.build(), title: "New Patron"});
 });
 
+// Create new patron
 router.post('/new', function(req, res, next){
   const bodyProp = req.body;
   Patrons.create(req.body).then(function(patrons) {
@@ -26,6 +26,38 @@ router.post('/new', function(req, res, next){
   });
 });
 
+// Update Patron
+router.put('/details/:id', function(req, res, next){
+  const bodyProp = req.body;
+  Patrons.findByPk(req.params.id).then(function(patrons) {
+    return patrons.update(req.body);
+  }).then(function(patrons){
+    res.redirect("/patrons");    
+  }).catch(function(err){
+    console.log(err);
+    Patrons.findByPk(req.params.id).then(function(patrons){
+      Loans.findAll({
+        where: {
+          patron_id: patrons.id
+        }, 
+        order: [["patron_id", "DESC"]],
+        include: [
+          {
+            association: "books",
+            attributes: ["title"]
+          },
+          {
+          association: "patrons",
+          attributes: ["first_name", "last_name"]
+          }
+        ]}).then(function(loans){
+          res.render('patron_detail',{bodyProp: bodyProp, loans: loans, patrons: patrons, err: err});
+        });
+    });
+  });
+});
+
+// Patron Details page
 router.get("/details/:id", function(req, res, next){
   Patrons.findByPk(req.params.id).then(function(patrons){
     Loans.findAll({
